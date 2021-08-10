e2cc6ef47b1b80116d0825b825759e57c5dbd3b3
kurlinemarie <88689096+kurlinemarie@users.noreply.github.com>
2021-08-10 15:28:25 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
 
-if [ -n "$GITHUB_EVENT_PATH" ];
+# if keyword is found
+if echo "$*" | grep -i -q MYKEYWORD;
 then
-    EVENT_PATH=$GITHUB_EVENT_PATH
-elif [ -f ./hello.py ];
-then
-    EVENT_PATH='.'
-    LOCAL_TEST=true
-    echo "found the python code"
-    exit 1
+    # do something
+    echo "Found keyword."
+    echo "Running python"
+    python3 hello.py
+# otherwise
 else
-    echo "No python code  to process! :("
-    exit 1
+    # exit gracefully
+    echo "Nothing to process."
 fi
